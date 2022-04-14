b44899e513c1166d1d355d44c4b6b849893c22ed
nvalerio12 <nelson.j.valerio@gmail.com>
2022-04-14 11:30:38 -0400

    added navigation bar

---
+++ src/helpers/navigationLinks.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        name: 'HOME',
+        ref: '#home'
+    },
+    {
+        name: 'ABOUT ME',
+        ref: '#about-me'
+    },
+    {
+        name: 'PORTFOLIOS',
+        ref: '#portfolio'
+    },
+    {
+        name: 'TECHNOLOGIES',
+        ref: '#technologies'
+    },
+    {
+        name: 'CONTACT',
+        ref: '#contact'
+    }
+]
